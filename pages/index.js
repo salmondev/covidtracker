9b1de6de9fa2a74031befbbe68c3a47224505522
Panurut <panuc129@syner.postbox.in.th>
2020-04-10 11:54:13 +0700

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,103 @@
+import useSWR from 'swr';
+import fetch from 'unfetch';
+import DataTable from 'react-data-table-component';
+
+const apiUrl = 'https://api.covid19api.com/summary';
+const timeSeriesUrl = 'https://pomber.github.io/covid19/timeseries.json';
+
+const fetcher = url => fetch(url).then(r => r.json());
+
+import columns from '../components/DataTableColumns';
+import DataChart from '../components/DataChart';
+import TimeSeriesChart from '../components/TimeSeriesChart';
+import Loading from '../components/Loading';
+
+const customStyle = {
+  tableWrapper: {
+    style: {
+      display: 'block'
+    }
+  }
+};
+
+const IndexPage = () => {
+  const { data, error } = useSWR(apiUrl, fetcher);
+  const { data: timeseries } = useSWR(timeSeriesUrl, fetcher);
+
+  if (!data) {
+    return <Loading />;
+  }
+
+  if (error) {
+    return <p>Error...</p>;
+  }
+
+  const sortedData = data.Countries.sort((a, b) =>
+    a.TotalConfirmed < b.TotalConfirmed ? 1 : -1
+  );
+
+  return (
+    <div className="container">
+      <style jsx>
+        {`
+          @import url('https://fonts.googleapis.com/css?family=Sarabun&display=swap');  
+
+          * {
+            font-family: 'Sarabun', sans-serif;
+          }
+
+          .container {
+            width: 820px;
+            margin: 0 auto;
+          }
+
+          .title {
+            text-align: center;
+          }
+
+          footer {
+            text-align: center;
+            padding: 2rem 0;
+          }
+        `}
+      </style>
+      <h2 className="title">COVID-19      Data || ข้อมูลการแพร่ระบาดเชื้อโควิด-๑๙</h2>
+
+      <DataTable
+        title="COVID-19 Summary"
+        customStyles={customStyle}
+        columns={columns}
+        data={sortedData}
+        pagination={true}
+      />
+
+      <DataChart data={data.Countries} title="Summary" />
+
+      <TimeSeriesChart data={timeseries.Thailand} title="Thailand Summary" />
+      <TimeSeriesChart data={timeseries.US} title="US Summary" />
+      <TimeSeriesChart data={timeseries.China} title="China Summary" />
+
+      <footer>
+        <p>
+          <a
+            href="https://github.com/salmondev"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            army    
+          </a>
+          {' @ '}
+          <a
+            href="https://panurut.dev"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            panurut.dev
+          </a>
+        </p>
+      </footer>
+    </div>
+  );
+};
+
+export default IndexPage;
